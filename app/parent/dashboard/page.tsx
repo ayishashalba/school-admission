e57b8c89b20476fc7ade5e50291bd31062661751
Ayisha Shalba <ayishashalba555@gmail.com>
2026-09-25 08:26:37 +0530

    fix parent dashboard dynamic rendering

--- app/parent/dashboard/page.tsx
+++ app/parent/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { connectDB } from "@/lib/mongodb";
 import Student from "@/models/Student";
-
+export const dynamic = "force-dynamic";
 export default async function ParentDashboard() {
   await connectDB();
 
